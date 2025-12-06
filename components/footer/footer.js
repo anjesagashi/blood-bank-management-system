ea897dca89369188153b0f8d24b6bf5032f1c217
anjesagashi <145193704+anjesagashi@users.noreply.github.com>
2025-12-06 21:22:23 +0100

    added footer component

---
+++ components/footer/footer.js
@@ -0,0 +1,58 @@
+class FooterComponent extends HTMLElement {
+  connectedCallback() {
+    this.render();
+  }
+  render() {
+    this.innerHTML = `
+          <footer class="footer">
+      <div class="socials">
+        <img
+          src="../../images/lifeFlow.png"
+          alt="Life Flow Image"
+          class="lifeFlowlogo"
+        />
+        <p>
+          Our mission is to save lives by connecting generous donors with those
+          in urgent need.
+        </p>
+        <div class="mediaIcons">
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            class="mediaIcon"
+          >
+            <img src="../../images/svg/instagram.svg" alt="Instagram Icon" />
+          </a>
+          <a href="https://www.facebook.com/" target="_blank" class="mediaIcon">
+            <img src="../../images/svg/facebook.svg" alt="Facebook Icon" />
+          </a>
+          <a
+            href="https://www.whatsapp.com/?lang=en"
+            target="_blank"
+            class="mediaIcon"
+          >
+            <img src="../../images/svg/whatsapp.svg" alt="Whatsapp Icon" />
+          </a>
+        </div>
+      </div>
+
+      <div class="footerSection">
+        <h4>Quick Links</h4>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Donate</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+
+      <div class="footerSection">
+        <h4>Contact</h4>
+        <p>Email: info@bloodbank.com</p>
+        <p>Phone: +383 XX XXX XXX</p>
+      </div>
+    </footer>
+        `;
+  }
+}
+customElements.define("custom-footer", FooterComponent);
